cf3919475cf18dd2873e6a3ebd7b89abd5efc228
jawnwa22 <jawnwa22@gmail.com>
2019-10-21 19:56:19 +0800

    init file

---
+++ app.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const route = require('koa-route');
+const app = new Koa();
+
+const redirect = ctx => {
+    ctx.response.redirect('/');
+}
+
+const main = (ctx) => {
+    ctx.response.body = "hello world";
+    console.log(ctx);
+    
+}
+
+app.use(route.get("/", main));
+app.use(route.get('/redirect', redirect));
+
+app.listen(3000);
